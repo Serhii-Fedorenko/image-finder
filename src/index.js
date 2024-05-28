c6cdef49eb005d65298c164c60ff9a7584726ba9
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-05-28 20:13:28 +0300

    setup open lightbox

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import getImages from './js/getImages';
 
 const gallery = document.getElementById('gallery');
 const form = document.querySelector('.search-form');
 
 form.addEventListener('submit', handleInputChange);
+gallery.addEventListener('click', handleGalleryItemClick);
 
 function renderImages(arr) {
   const murkup = arr
     .map(
       item =>
-        `<a class='gallery_item' href='${item.largeImageURL}' data-source='${item.largeImageURL}'><img src='${item.webformatURL}' alt='${item.tags}'/></a>`
+        `<a class='gallery_item' href='${item.largeImageURL}' data-source='${item.largeImageURL}'><img src='${item.webformatURL}' alt='${item.tags}' loading="lazy"/></a>`
     )
     .join('');
   return gallery.insertAdjacentHTML('beforeend', murkup);
@@ -17,12 +20,28 @@ function renderImages(arr) {
 
 function handleInputChange(e) {
   e.preventDefault();
-  gallery.innerHTML = ''
+  gallery.innerHTML = '';
   const form = e.target;
   const currentPage = 1;
-  console.dir(form.elements.query.value);
   getImages(form.elements.query.value, 1).then(({ hits }) =>
     renderImages(hits)
   );
   form.reset();
+}
+
+function handleGalleryItemClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') return;
+  const currentImgOriginal = e.target.src;
+  handleShowImage(currentImgOriginal)
+}
+
+function handleShowImage(src) {
+  const lightbox = new SimpleLightbox('.gallery_item', {
+    sourceAttr: 'data-source',
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.5
+  });
+  lightbox.open([src]);
 }
